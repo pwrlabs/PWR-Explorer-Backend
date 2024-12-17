3810c583d3cd902baeb7d4ae03c784cc3029f076
johnjurdak <jjurdak@netvariant.com>
2024-12-17 13:23:09 +0200

    testing memory leak fix

--- src/main/java/Block/Block.java
+++ src/main/java/Block/Block.java
@@ -1,27 +1,36 @@
 package Block;
 
-
 import Txn.NewTxn;
 import Txn.Txns;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static Core.Sql.Queries.getBlockTxns;
 
 public class Block {
-    private final Logger logger = LogManager.getLogger(Block.class);
+    private static final Logger logger = LogManager.getLogger(Block.class);
+    private static final int MAX_TRANSACTIONS_IN_MEMORY = 1000;
+
     private String blockNumber;
     private final long timeStamp;
-    private String  blockSubmitter;
+    private String blockSubmitter;
     private long blockReward;
     private int blockSize;
     private int txnCount;
 
-    private final List<NewTxn> txns;
+    // Use lazy loading for transactions
+    private volatile List<NewTxn> txns;
+    private final Object txnLock = new Object();
 
-    public Block(String blockNumber, long timeStamp, String blockSubmitter, long blockReward, int blockSize, int txnCount) {
+    // Keep track of number of blocks in memory
+    private static final AtomicInteger activeBlocks = new AtomicInteger(0);
+
+    public Block(String blockNumber, long timeStamp, String blockSubmitter,
+                 long blockReward, int blockSize, int txnCount) {
         this.blockNumber = blockNumber;
         this.timeStamp = timeStamp;
         this.blockSubmitter = blockSubmitter;
@@ -29,15 +38,80 @@ public class Block {
         this.blockSize = blockSize;
         this.txnCount = txnCount;
 
-        txns = getBlockTxns(blockNumber);
+        // Don't load transactions in constructor
+        activeBlocks.incrementAndGet();
+        logger.debug("Created new Block {}. Active blocks: {}", blockNumber, activeBlocks.get());
 
+        // Add to blocks collection
         Blocks.add(this);
     }
 
     public void addTxn(NewTxn txn, int positionInTheBlock) {
-        txns.add(positionInTheBlock, txn);
+        if (txn == null) {
+            logger.warn("Attempted to add null transaction to block {}", blockNumber);
+            return;
+        }
+
+        initializeTransactionsIfNeeded();
+
+        try {
+            if (positionInTheBlock >= 0 && positionInTheBlock <= txns.size()) {
+                txns.add(positionInTheBlock, txn);
+                txnCount = txns.size();
+                logger.debug("Added transaction at position {} in block {}", positionInTheBlock, blockNumber);
+            } else {
+                logger.warn("Invalid position {} for transaction in block {}",
+                        positionInTheBlock, blockNumber);
+            }
+        } catch (Exception e) {
+            logger.error("Error adding transaction to block {}: {}", blockNumber, e.getMessage(), e);
+        }
     }
 
+    private void initializeTransactionsIfNeeded() {
+        if (txns == null) {
+            synchronized (txnLock) {
+                if (txns == null) {
+                    try {
+                        logger.debug("Loading transactions for block {}", blockNumber);
+                        List<NewTxn> loadedTxns = getBlockTxns(blockNumber);
+
+                        if (loadedTxns == null) {
+                            loadedTxns = new ArrayList<>();
+                            logger.warn("No transactions loaded for block {}, initializing empty list", blockNumber);
+                        }
+
+                        // If too many transactions, warn about potential memory issues
+                        if (loadedTxns.size() > MAX_TRANSACTIONS_IN_MEMORY) {
+                            logger.warn("Block {} contains {} transactions, which exceeds the recommended limit of {}",
+                                    blockNumber, loadedTxns.size(), MAX_TRANSACTIONS_IN_MEMORY);
+                        }
+
+                        txns = new ArrayList<>(loadedTxns);
+                        txnCount = txns.size();
+                    } catch (Exception e) {
+                        logger.error("Error initializing transactions for block {}: {}",
+                                blockNumber, e.getMessage(), e);
+                        txns = new ArrayList<>(); // Initialize empty list on error
+                    }
+                }
+            }
+        }
+    }
+
+    // Cleanup method to be called when block is no longer needed
+    public void cleanup() {
+        synchronized (txnLock) {
+            if (txns != null) {
+                txns.clear();
+                txns = null;
+            }
+        }
+        activeBlocks.decrementAndGet();
+        logger.debug("Cleaned up Block {}. Active blocks: {}", blockNumber, activeBlocks.get());
+    }
+
+    // Getters and setters
     public String getBlockNumber() {
         return blockNumber;
     }
@@ -83,71 +157,33 @@ public class Block {
     }
 
     public NewTxn[] getTxns() {
+        initializeTransactionsIfNeeded();
         return txns.toArray(new NewTxn[0]);
     }
 
-    //    public Logger getLogger() {
-//        return logger;
-//    }
-//
-//    public String getBlockNumber() {
-//        return blockNumber;
-//    }
-//
-//    public void setBlockNumber(String blockNumber) {
-//        this.blockNumber = blockNumber;
-//    }
-//
-//    public void setBlockSubmitter(byte[] blockSubmitter) {
-//        this.blockSubmitter = blockSubmitter;
-//    }
-//
-//    public void setBlockReward(long blockReward) {
-//        this.blockReward = blockReward;
-//    }
-//
-//    public void setBlockSize(int blockSize) {
-//        this.blockSize = blockSize;
-//    }
-//
-//    public int getTxnCount() {
-//        return txnCount;
-//    }
-//
-//    public void setTxnCount(int txnCount) {
-//        this.txnCount = txnCount;
-//    }
-//
-//    public long getTimeStamp() {
-//        return timeStamp;
-//    }
+    // Get active blocks count - useful for monitoring
+    public static int getActiveBlocksCount() {
+        return activeBlocks.get();
+    }
 
-//    public byte[] getBlockSubmitter() {
-//        return load("blockSubmitter");
-//    }
-//    public byte[] getBlockSubmitter() {
-//        return blockSubmitter;
-//    }
-//    public long getBlockReward() {
-//        return loadLong("blockReward");
-//    }
-//    public long getBlockReward() {
-//        return blockReward;
-//    }
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            cleanup();
+        } finally {
+            super.finalize();
+        }
+    }
 
-//    public int getBlockSize() {
-//        return loadInt("blockSize");
-//    }
-
-//    public int getBlockSize() {
-//        return blockSize;
-//    }
-//    public int getTxnsCount() {
-//        if(txns == null) return loadInt("txnCount");
-//        else return txns.length;
-//    }
-//    public Txn[] getTxns() {
-//        return txns;
-//    }
-
+    @Override
+    public String toString() {
+        return "Block{" +
+                "blockNumber='" + blockNumber + '\'' +
+                ", timeStamp=" + timeStamp +
+                ", blockSubmitter='" + blockSubmitter + '\'' +
+                ", blockReward=" + blockReward +
+                ", blockSize=" + blockSize +
+                ", txnCount=" + txnCount +
+                '}';
+    }
 }

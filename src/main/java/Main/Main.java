178599fc288f6fb57338d396038d580ee4a94317
Amir El Halabi <aelhalabi@shelvz.com>
2025-06-13 14:18:08 +0300

    fix: port fix

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -23,7 +23,7 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws NoSuchMethodException, IOException, SQLException {
-        port(8082);
+        port(8081);
 
         options("/*",
                 (request, response) -> {

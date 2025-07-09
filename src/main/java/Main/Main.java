3ca65330aa804a0fe8ac1622400db9d28abf6e18
Ghadi <yobiz668@gmail.com>
2025-07-09 09:41:36 +0300

    check

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -88,6 +88,7 @@ public class Main {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Shutting down... Stopping synchronizer");
             Synchronizer.stop();
+
         }));
     }
 

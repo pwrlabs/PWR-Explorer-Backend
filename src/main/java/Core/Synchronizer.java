03119d8e803737c3472c169fdce03a49e1947137
Ghadi <yobiz668@gmail.com>
2025-07-15 14:12:24 +0300

    additions

--- src/main/java/Core/Synchronizer.java
+++ src/main/java/Core/Synchronizer.java
@@ -24,6 +24,7 @@ public class Synchronizer {
     public static void sync(PWRJ pwrj) {
         running = true;
         long blockToCheck = Math.max(getLastStoredBlock() + 1, 1);
+        blockToCheck = 24259;
         logger.info("Synchronizer starting at block {}", blockToCheck);
 
         while (running) {
